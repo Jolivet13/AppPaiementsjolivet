91c6d85bca134d643eb982dce248d1285ccff49e
fon <jolivet.fonkenfack@monboreal.ca>
2024-11-15 14:23:42 -0500

    creation de la branche dev-utilisateur et de la classe Utilisateur

---
+++ AppPaiementsfon/AppPaiementsfon/Utilisateur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiementsfon
+{
+    internal class Utilisateur : Paiements
+    {
+        public string Nom { get; set; }
+        public List<Paiements> Paiements { get; set; }
+        public Utilisateur(decimal montant, string description, string nom) : base((double)montant, description)
+        {
+            Nom = nom;
+            Paiements = new List<Paiements>();
+        }
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom: {Nom}");
+            Console.WriteLine("Paiements:");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+        public void AjouterPaiement(Paiements paiement)
+        {
+            Paiements.Add(paiement);
+        }
+    }
+}
