673fb89068d2308d53b38eb1b8ba332b91d6644e
fon <jolivet.fonkenfack@monboreal.ca>
2024-11-15 13:51:03 -0500

    creation de la branche dev-cartecredit et de la classe Cartecredit

---
+++ AppPaiementsfon/AppPaiementsfon/Cartecredit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiementsfon
+{
+    internal class Cartecredit:Paiements
+    {
+        public int NumeroCarte { get; set; }
+        public Cartecredit(decimal montant, string description, int Carte)
+        : base((double)montant, description)
+        { NumeroCarte = Carte; }
+        public new virtual void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Numéro de Carte: {NumeroCarte}");
+
+        }
+    }
+}
