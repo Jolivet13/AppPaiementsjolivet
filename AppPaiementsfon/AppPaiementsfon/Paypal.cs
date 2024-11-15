120a1e8725eac97e063f48a5a87e583906cc21c7
fon <jolivet.fonkenfack@monboreal.ca>
2024-11-15 14:07:07 -0500

    creation de la branche dev-paypal et dela classe fille Paypal

---
+++ AppPaiementsfon/AppPaiementsfon/Paypal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiementsfon
+{
+    internal class Paypal:Paiements
+    {
+        public string Courriel { get; set; }
+        public Paypal(decimal montant, string description, string email)
+            : base((double)montant, description)
+        {
+            Courriel = email;
+        }
+        public new virtual void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Email: {Courriel}");
+        }
+    }
+}
--- AppPaiementsfon/AppPaiementsfon/Program.cs
+++ AppPaiementsfon/AppPaiementsfon/Program.cs
@@ -8,13 +8,18 @@ internal class Program
         var achat_steve = new Cartecredit(250.00m, " Achat de livres ", 234567890);
         var achat_fon = new Cartecredit(300.00m, " Achat de nourritures ", 345678901);
         List<Cartecredit> achats = new List<Cartecredit> { achat_jolivet, achat_steve, achat_fon };
-
         foreach (var achat in achats)
         {
             achat.AfficherDetails();
             Console.WriteLine();
-
         }
-
+        var paypal1 = new Paypal(750.00m, "Achat de logiciels", "kenfackjolivet@gmail.com");
+        var paypal2 = new Paypal(100.00m, "Paiement abonnement", "jolivet.fonkenfack@monboreal.ca");
+        List<Paypal> paiements = new List<Paypal> { paypal1, paypal2 };
+        foreach (var paiement in paiements)
+        {
+            paiement.AfficherDetails();
+            Console.WriteLine();
+        }
     }
 }
